884aef408c4b1b8a497c0e6bb35a7285485b9cd6
Vitor Gomes Guimarães <vitorgguimaraes56@gmail.com>
2026-05-04 12:08:08 -0300

    commit

--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { JwtService } from '@nestjs/jwt';
-export declare class AuthService {
-    private jwtService;
-    constructor(jwtService: JwtService);
-    login(username: string, password: string): Promise<{
-        access_token: string;
-    }>;
-}
